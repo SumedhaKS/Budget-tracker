8091e3897fa46714291ffddf1853656a0fbec9d3
SumedhaKS <kssumedha24@gmail.com>
2025-05-26 10:55:36 +0530

    resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Form from './Form';
 
@@ -10,32 +9,6 @@ function App() {
         <Route path="/" element={<Form />} />
       </Routes>
     </BrowserRouter>
-=======
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Form from './form';
-import Expense from './DLform';
-import './App.css';
-
-function App() {
-  return (
-    <div
-      style={{
-        backgroundImage: `url('download.jpeg')`, // Put your image in public folder
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        minHeight: '100vh',
-      }}
-    >
-      <Router>
-        <Routes>
-          <Route path="/" element={<Form />} />
-          <Route path="/home" element={<Expense />} />
-        </Routes>
-      </Router>
-    </div>
->>>>>>> Stashed changes
   );
 }
 
